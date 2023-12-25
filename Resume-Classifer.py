3a86da4d17ac3f94dbeba724d419737bdfc35e9a
Gurram vijaya lakshmi <106443233+gurramvijayalakshmi@users.noreply.github.com>
2023-12-25 20:22:14 +0530

    Update Resume-Classifer.py

--- Resume-Classifer.py
+++ Resume-Classifer.py
@@ -1,1 +1,90 @@
 
+import os
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
+from tensorflow.keras.models import Model
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report
+import matplotlib.pyplot as plt
+
+# Set random seed for reproducibility
+np.random.seed(42)
+tf.random.set_seed(42)
+
+# Set up paths and parameters
+data_dir = "path/to/dataset"
+resume_dir = os.path.join(data_dir, "resume")
+non_resume_dir = os.path.join(data_dir, "non_resume")
+
+# Function to load and preprocess images
+def load_and_preprocess_images(directory, label):
+    images = []
+    labels = []
+    for filename in os.listdir(directory):
+        if filename.endswith(".jpg") or filename.endswith(".png"):
+            img_path = os.path.join(directory, filename)
+            img = image.load_img(img_path, target_size=(224, 224))
+            img_array = image.img_to_array(img)
+            img_array = preprocess_input(img_array)
+            images.append(img_array)
+            labels.append(label)
+    return np.array(images), np.array(labels)
+
+# Load and preprocess images
+resume_images, resume_labels = load_and_preprocess_images(resume_dir, 1)
+non_resume_images, non_resume_labels = load_and_preprocess_images(non_resume_dir, 0)
+
+# Combine resume and non-resume data
+all_images = np.concatenate([resume_images, non_resume_images], axis=0)
+all_labels = np.concatenate([resume_labels, non_resume_labels], axis=0)
+
+# Split data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(all_images, all_labels, test_size=0.2, random_state=42)
+
+# Load pre-trained VGG16 model without top layers
+base_model = VGG16(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+
+# Freeze the pre-trained layers
+for layer in base_model.layers:
+    layer.trainable = False
+
+# Add custom classification layers
+x = base_model.output
+x = tf.keras.layers.GlobalAveragePooling2D()(x)
+x = tf.keras.layers.Dense(128, activation='relu')(x)
+predictions = tf.keras.layers.Dense(1, activation='sigmoid')(x)
+
+# Create the model
+model = Model(inputs=base_model.input, outputs=predictions)
+
+# Compile the model
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Train the model
+history = model.fit(X_train, y_train, epochs=10, validation_split=0.2)
+
+# Evaluate the model
+test_loss, test_acc = model.evaluate(X_test, y_test)
+print(f"Test Accuracy: {test_acc}")
+
+# Confusion Matrix
+predictions = (model.predict(X_test) > 0.5).astype("int32")
+conf_matrix = confusion_matrix(y_test, predictions)
+print("Confusion Matrix:")
+print(conf_matrix)
+
+# Classification Report
+class_names = ['Non-Resume', 'Resume']
+report = classification_report(y_test, predictions, target_names=class_names)
+print("Classification Report:")
+print(report)
+
+# Plot training history
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label='val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend(loc='lower right')
+plt.show()
